4d8d6843362d8b5e40272205799df1ec20df3f1b
Christopher Clemm <54604131+ChristopherClemm@users.noreply.github.com>
2020-03-03 11:45:47 -0800

    uploaded files

---
+++ header/Command.hpp
@@ -0,0 +1,28 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <unistd.h>
+#include <stdio.h>
+
+using namespace std;
+class Command{
+
+
+        public:
+                Command* leftChild = NULL;
+                Command* rightChild = NULL;
+  virtual bool execute() = 0;
+  virtual void setFileName(string s) = 0;
+
+
+
+};
+#endif
+
